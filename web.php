3d6e0e741e0dbd5f1cbb8c4e40b426f876ed9b01
Zeynep Erarslan <120674682+zeyneperarslan@users.noreply.github.com>
2024-05-28 23:28:27 +0300

    Update web.php

--- web.php
+++ web.php
@@ -1,8 +1,8 @@
 <?php
-               $server = 'localhost';
-               $user = 'root';
-               $password = '';
-               $database = 'resimkulup';
+               $server = 'sql111.infinityfree.com';
+               $user = 'if0_36633270';
+               $password = '3TuHdMWIcw2LIcO';
+               $database = 'if0_36633270_resimkulup';
                $baglanti = mysqli_connect($server,$user,$password,$database);
 if (!$baglanti) {
 echo "MySQL sunucu ile baglanti kurulamadi! </br>"; echo "HATA: " . mysqli_connect_error();
