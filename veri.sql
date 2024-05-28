d6bb76993c1fb8fcdc36d1a7e83fd016e777a3aa
Zeynep Erarslan <120674682+zeyneperarslan@users.noreply.github.com>
2024-05-28 16:50:28 +0300

    Add files via upload

---
+++ veri.sql
@@ -0,0 +1,37 @@
+
+CREATE DATABASE IF NOT EXISTS resimkulup; --veritabanını oluşturdum
+
+--tabloları oluşturdum 
+
+CREATE TABLE uyeler(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    kullaniciadi VARCHAR(255) NOT NULL,
+    sifre VARCHAR(255) NOT NULL,
+    isim VARCHAR(255) NOT NULL,
+    soyisim VARCHAR(255) NOT NULL,
+    telefon VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    adres VARCHAR(250) NOT NULL,
+    UNIQUE (kullaniciadi)
+);
+
+
+CREATE TABLE etkinlikler(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    etkinlik_adı VARCHAR(255) NOT NULL,
+    tarih DATE ,
+    yer VARCHAR(255) NOT NULL ,
+    konu VARCHAR(255) NOT NULL 
+
+);
+
+CREATE TABLE malzemeler(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    malzemeadi VARCHAR(255) NOT NULL ,
+    tur VARCHAR(255) NOT NULL ,
+    sayi VARCHAR(255) NOT NULL 
+);
+
+
+
+
